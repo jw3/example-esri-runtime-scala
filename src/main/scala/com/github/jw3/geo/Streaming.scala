b6c6722c3a74fb497435c2014eb019f9d1a6c01e
John Wass <wassj@ctc.com>
2018-07-08 16:35:24 -0400

    example of labeling with timed states

--- src/main/scala/com/github/jw3/geo/Streaming.scala
+++ src/main/scala/com/github/jw3/geo/Streaming.scala
@@ -6,13 +6,14 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.ws.{Message, TextMessage, WebSocketRequest}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
+import com.esri.arcgisruntime.arcgisservices.LabelDefinition
 import com.esri.arcgisruntime.geometry.{Point, SpatialReferences}
 import com.esri.arcgisruntime.mapping.view.{Graphic, GraphicsOverlay}
 import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol
 import com.github.jw3.geo.GraphicActor.MoveTo
 
-import scala.concurrent.duration.DurationInt
 import scala.concurrent.Promise
+import scala.concurrent.duration.{DurationInt, FiniteDuration}
 
 object Streaming {
   def connect(o: GraphicsOverlay, to: String)(implicit sys: ActorSystem): Promise[Option[Message]] = {
@@ -57,20 +58,44 @@ object GraphicActor {
 
   val defaultMarker = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, -0x00100, 10f)
   val defaultInactiveMarker = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.X, -0x10000, 8f)
+  val inactiveThreshold = 15.seconds
 
   case object Inactivity
+  case object InactiveTimerKey
+  case class InactivityUpdate(t: FiniteDuration)
 
   case class MoveTo(x: Double, y: Double)
 
+  val defaultLabelDefinition = """{
+                                 |  "labelExpressionInfo":
+                                 |  {
+                                 |    "expression": "$feature.lastUpdate"
+                                 |  },
+                                 |  "labelPlacement": "esriServerPointLabelPlacementAboveRight",
+                                 |  "symbol":
+                                 |  {
+                                 |    "color": [255,0,0,123],
+                                 |    "font": {
+                                 |      "size": 16,
+                                 |      "weight": "bold"
+                                 |    },
+                                 |    "type": "esriTS"
+                                 |  }
+                                 |}""".stripMargin
 }
 
 class GraphicActor(id: String, o: GraphicsOverlay) extends Actor with Timers with Stash with ActorLogging {
+  import GraphicActor._
+
   private val sr = SpatialReferences.getWgs84
 
   def unlocated: Receive = {
     case MoveTo(x, y) ⇒
       val pt = new Point(x, y, sr)
-      val g = new Graphic(pt, GraphicActor.defaultMarker)
+      val g = new Graphic(pt, defaultMarker)
+
+      o.getLabelDefinitions.add(LabelDefinition.fromJson(defaultLabelDefinition))
+      o.setLabelsEnabled(true)
 
       o.getGraphics.add(g)
       context.become(active(g))
@@ -81,23 +106,30 @@ class GraphicActor(id: String, o: GraphicsOverlay) extends Actor with Timers wit
 
     {
       case MoveTo(x, y) ⇒
-        timers.startSingleTimer(id, GraphicActor.Inactivity, 30.seconds)
+        timers.startSingleTimer(InactiveTimerKey, InactivityUpdate(inactiveThreshold), inactiveThreshold)
         g.setGeometry(new Point(x, y, sr))
 
-      case GraphicActor.Inactivity ⇒
+      case InactivityUpdate(t) ⇒
+        stash()
         context.become(inactive(g))
     }
   }
 
   def inactive(g: Graphic): Receive = {
     val s = g.getSymbol
-    g.setSymbol(GraphicActor.defaultInactiveMarker)
+    g.setSymbol(defaultInactiveMarker)
+    unstashAll()
 
     {
       case MoveTo(_, _) ⇒
         stash()
         g.setSymbol(s)
         context.become(active(g))
+
+      case InactivityUpdate(t) ⇒
+        g.getAttributes.put("lastUpdate", t.toString)
+        val d = if (t < 1.minute) 5.seconds else 1.minute
+        timers.startSingleTimer(InactiveTimerKey, InactivityUpdate(t + d), d)
     }
   }
 
