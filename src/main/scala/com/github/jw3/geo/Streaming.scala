85b1ee7dc92848a23d12d68a15fc705d53fd06f8
John Wass <wassj@ctc.com>
2018-07-08 00:06:44 -0400

    example of timed states on symbols

--- src/main/scala/com/github/jw3/geo/Streaming.scala
+++ src/main/scala/com/github/jw3/geo/Streaming.scala
@@ -1,7 +1,7 @@
 package com.github.jw3.geo
 
 import akka.Done
-import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props, Stash, Timers}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.ws.{Message, TextMessage, WebSocketRequest}
 import akka.stream.ActorMaterializer
@@ -11,6 +11,7 @@ import com.esri.arcgisruntime.mapping.view.{Graphic, GraphicsOverlay}
 import com.esri.arcgisruntime.symbology.SimpleMarkerSymbol
 import com.github.jw3.geo.GraphicActor.MoveTo
 
+import scala.concurrent.duration.DurationInt
 import scala.concurrent.Promise
 
 object Streaming {
@@ -54,12 +55,16 @@ class OverlayActor(o: GraphicsOverlay) extends Actor {
 object GraphicActor {
   def props(id: String, o: GraphicsOverlay) = Props(new GraphicActor(id, o))
 
-  val defaultMarker = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, -0x10000, 10f)
+  val defaultMarker = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.CIRCLE, -0x00100, 10f)
+  val defaultInactiveMarker = new SimpleMarkerSymbol(SimpleMarkerSymbol.Style.X, -0x10000, 8f)
+
+  case object Inactivity
 
   case class MoveTo(x: Double, y: Double)
+
 }
 
-class GraphicActor(id: String, o: GraphicsOverlay) extends Actor with ActorLogging {
+class GraphicActor(id: String, o: GraphicsOverlay) extends Actor with Timers with Stash with ActorLogging {
   private val sr = SpatialReferences.getWgs84
 
   def unlocated: Receive = {
@@ -68,12 +73,32 @@ class GraphicActor(id: String, o: GraphicsOverlay) extends Actor with ActorLoggi
       val g = new Graphic(pt, GraphicActor.defaultMarker)
 
       o.getGraphics.add(g)
-      context.become(positioned(g))
+      context.become(active(g))
   }
 
-  def positioned(g: Graphic): Receive = {
-    case MoveTo(x, y) ⇒
-      g.setGeometry(new Point(x, y, sr))
+  def active(g: Graphic): Receive = {
+    unstashAll()
+
+    {
+      case MoveTo(x, y) ⇒
+        timers.startSingleTimer(id, GraphicActor.Inactivity, 30.seconds)
+        g.setGeometry(new Point(x, y, sr))
+
+      case GraphicActor.Inactivity ⇒
+        context.become(inactive(g))
+    }
+  }
+
+  def inactive(g: Graphic): Receive = {
+    val s = g.getSymbol
+    g.setSymbol(GraphicActor.defaultInactiveMarker)
+
+    {
+      case MoveTo(_, _) ⇒
+        stash()
+        g.setSymbol(s)
+        context.become(active(g))
+    }
   }
 
   def receive: Receive = unlocated
